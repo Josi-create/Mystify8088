7799f5894fca4a11f6cf2f280efae4b079c7395b
Josi-create <johanneswack@web.de>
2024-08-22 19:40:19 +0200

    lib subdir deleted

--- lib/mach.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo --------------------------------
-tasm %1
-echo --------------------------------
-tcc -c %2
-echo --------------------------------
-tlink c0s %1 %2,%2,,cs emu maths graphics > error.log
-type error.log
-echo ---------------------------- ----
